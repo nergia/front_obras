7dc93185246b93216504b5da5cc65ac56172c0fa
nergia <matheussr000@gmail.com>
2024-01-22 16:46:07 -0300

    Delete change

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -8,6 +8,7 @@
                     <th scope="col">Email</th>
                     <th scope="col">Designation</th>
                     <th scope="col">Contact No.</th>
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody v-for="contact in contacts" :key="contact.id">
@@ -17,6 +18,7 @@
                     <th scope="row">{{contact.email}}</th>
                     <th scope="row">{{contact.designation}}</th>
                     <th scope="row">{{contact.contact_no}}</th>
+                    <th scope="row"><button class="btn btn-danger btn-sm" @click.prevent="deleteContact(contact.id)">Delete</button></th>
                 </tr>
             </tbody>
         </table>
@@ -45,6 +47,15 @@ export default {
             }).catch(error=>{
                 console.log(error);
             });
+        },
+        async deleteContact(id){
+            let url="http://127.0.0.1:8000/api/delete_contact/${id}";
+            await axios.get(url).then(response => {
+                if(response.data.code ==200){
+                    alert(response.message);
+                    this.getContacts();
+                }
+            }).catch()
         }
     },
     mounted(){
